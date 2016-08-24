689a5e6bb86d54e5163d7f46e913668672e0220c
Indranil Sen <indranilsen010@gmail.com>
2016-08-24 17:00:35 -0400

    Update make-webapp.rb

--- make-webapp.rb
+++ make-webapp.rb
@@ -1,6 +1,6 @@
 class MakeWebapp < Formula
   homepage "https://github.com/indranilsen/make-webapp"
-  url "https://github.com/indranilsen/make-webapp/archive/1.1.0.tar.gz"
+  url "https://github.com/indranilsen/make-webapp/archive/1.1.1.tar.gz"
   version "1.1.1"
   sha256 "8bbc36efbdeee1aaf0dec1e979979ce01dace5b656a5c6d17145093bf6f48d9d"
 
